c950ec806a98b79a7043ec11c3a1e36cbfcb63ac
Roman Flössler <flor@flor.cz>
2018-08-01 20:03:32 +0200

    image up or down move, centering image, scroll mouse help removed

--- ssg.js
+++ ssg.js
@@ -4,20 +4,22 @@
 
 var SSG = {};  // main object - namespace
 
-SSG.initGallery = function initGallery(tag) {
+SSG.initGallery = function initGallery() {
     SSG.imgs = [];  // array of objects where image attributes are stored
     jQuery("body").append("<div id='SSG_galBg'></div> <div id='SSG_gallery'></div> <div id='SSG_exit'><span>&times;</span></div>"); // gallery's divs
-    if (tag == "DT") jQuery("#SSG_gallery").append("<a title='rolujte dolů'><div id='help'><div id='mouse'> <div id='scroll'></div> <div id='down'>&raquo;</div> </div></div></a>");
-    // if gallery thumbnail is wrapped in DT tag user will see visual help how to use gallery - Wordpress use a DT tag in its gallery
-    jQuery(document).keydown(function (event) {
-        if (event.which == 27) SSG.destroyGallery(); //ESC key destroys gallery
-    });
-    jQuery(document).keydown(function (event) {
-        if (event.which == 32) SSG.isSpaceBarPressed = true; // SpaceBar set a property that causes jumping on next photo
-        event.preventDefault();
-        event.stopPropagation();
-    });
+    jQuery("body").append("<div id='SSG_arrows'><div id='tip'>You can use also arrow keys</div><span class='up'></span><span class='down'></span></div>"); // gallery's arrows
+    jQuery(document).keydown(SSG.keyFunction);
     jQuery("#SSG_exit").click(SSG.destroyGallery);
+    jQuery("#SSG_arrows .up").click(function () { SSG.imageUp = true });
+    jQuery("#SSG_arrows .down").click(function () { SSG.imageDown = true });
+}
+
+SSG.keyFunction = function (event) {
+    if (event.which == 27) SSG.destroyGallery(); //ESC key destroys gallery
+    if (event.which == 40) { SSG.imageDown = true; jQuery("#SSG_arrows").remove(); } // SpaceBar set a property that causes jumping on next photo
+    if (event.which == 38) { SSG.imageUp = true; jQuery("#SSG_arrows").remove(); } // SpaceBar set a property that causes jumping on next photo        
+    event.preventDefault();
+    event.stopPropagation();
 }
 
 SSG.getImgList = function (clickedHref, clickedAlt) {
@@ -43,7 +45,9 @@ SSG.setVariables = function () {
     window.scrollTo(0, 0);
     SSG.scrHeight = jQuery(window).height();
     jQuery(window).width() / SSG.scrHeight >= 1 ? SSG.scrFraction = 2 : SSG.scrFraction = 4;  // different screen fraction for different screen aspect ratios
-    SSG.isSpaceBarPressed = false;
+    SSG.imageDown = false;
+    SSG.imageUp = false;
+    SSG.firstImage = true;
     SSG.addImage();
 }
 
@@ -90,32 +94,56 @@ SSG.checkLoading = function () {
         if (i < SSG.imgs.length - 1) { topPos = SSG.imgs[i + 1].pos } else { topPos = SSG.imgs[i].pos + SSG.scrHeight }
         if ((actual > SSG.imgs[i].pos) && (actual < topPos)) {
             if (typeof ga !== 'undefined') {
-                SSG.displayed != i && ga('send', 'pageview', '/img'+location.pathname+SSG.getName(SSG.imgs[i].href));
+                SSG.displayed != i && ga('send', 'pageview', '/img' + location.pathname + SSG.getName(SSG.imgs[i].href));
             } // sends pageview of actual image to Google Analytics
-            //      SSG.displayed != i && console.log('/img'+location.pathname+SSG.getName(SSG.imgs[i].href));
+            SSG.displayed != i && console.log('/img' + location.pathname + SSG.getName(SSG.imgs[i].href));
             SSG.displayed = i;
         }
     }
-    if (SSG.displayed+1 < SSG.imgs.length) {
-        if (SSG.isSpaceBarPressed && SSG.imgs[SSG.displayed+1].pos) window.scrollTo(0, SSG.imgs[SSG.displayed+1].pos-20);
-    } else {
-        SSG.isSpaceBarPressed && window.scrollTo(0, jQuery("#back").offset().top+100);
+
+    
+    if (SSG.displayed == 0 && SSG.imgs[0].pos && SSG.firstImage) {
+        window.scrollTo(0, SSG.imgs[0].pos - SSG.countImageIndent(0));
+        SSG.firstImage = false;
     }
-    SSG.isSpaceBarPressed = false;
+
+    if (SSG.imageUp && SSG.displayed - 1 >= 0)
+        jQuery("html, body").animate({ scrollTop: SSG.imgs[SSG.displayed - 1].pos - SSG.countImageIndent(SSG.displayed) + "px" }, 300, "swing");
+
+        
+    if (SSG.displayed + 1 < SSG.imgs.length) {
+        if (SSG.imageDown && SSG.imgs[SSG.displayed + 1].pos)
+            jQuery("html, body").animate({ scrollTop: SSG.imgs[SSG.displayed + 1].pos - SSG.countImageIndent(SSG.displayed) + "px" }, 300, "swing");
+    } else {
+        SSG.imageDown && jQuery("html, body").animate({ scrollTop: jQuery("#back").offset().top + 100 }, 200, "swing");
+    }
+    SSG.imageDown = false;
+    SSG.imageUp = false;
+}
+
+SSG.countImageIndent = function (index) {
+    var screen = jQuery(window).height();;
+    var img = jQuery("#i" + index).outerHeight(true);
+    var pIn = jQuery("#p" + index).innerHeight();
+    var pOut = jQuery("#p" + index).outerHeight(true);
+    var pMargin = pOut - pIn;
+    var centerPos = Math.round((screen - (img + pIn)) / 2);
+    return centerPos > pMargin ? pMargin : centerPos;
 }
 
 SSG.destroyGallery = function () {
     clearInterval(SSG.loading);
     if (typeof ga !== 'undefined') ga('send', 'pageview', location.pathname);
-    //    console.log(location.pathname);
-    jQuery("#SSG_galBg,#SSG_gallery,#SSG_exit").remove();
+    console.log(location.pathname);
+    jQuery("#SSG_galBg,#SSG_gallery,#SSG_exit,#SSG_arrows").remove();
     jQuery(window).off("resize", SSG.countResize);
+    jQuery(document).off("keydown", SSG.keyFunction);
     window.scrollTo(0, SSG.pos); // sets the original (before initGallery) vertical scroll of page    
 }
 
 SSG.run = function (event) {
-    SSG.initGallery(event.currentTarget.parentNode.tagName); // event pass a lot of data about clicked A tag
-    SSG.getImgList(event.currentTarget.href, event.currentTarget.children["0"].alt);
+    SSG.initGallery(); // event pass a lot of data about clicked A tag
+    event ? SSG.getImgList(event.currentTarget.href, event.currentTarget.children["0"].alt) : SSG.getImgList();
     SSG.setVariables();
     SSG.loading = setInterval(SSG.checkLoading, 300); // every 300 ms check if more images should be loaded
     jQuery(window).resize(SSG.countResize);
